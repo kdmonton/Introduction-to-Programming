3603fe481a4f873da7997f2f7504273d24a2a54b
Gawlooo <236571899+kdmonton@users.noreply.github.com>
2025-10-31 13:10:19 +0400

    Update Exercise8.py

--- ExerciseSumAssess1/Exercise8.py
+++ ExerciseSumAssess1/Exercise8.py
@@ -4,5 +4,5 @@ search = input('Enter the name you are looking for: ')
 if search.capitalize() in names:
     print (f'{search} is in the list')
 else:
-
     print (f'{search} is not in the list')
+
