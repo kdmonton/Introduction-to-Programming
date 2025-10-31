33b14d4c7b515e6b891a84bf9039eab1de3dd908
Gawlooo <236571899+kdmonton@users.noreply.github.com>
2025-10-31 12:51:21 +0400

    Update Exercise8.py

--- ExerciseSumAssess1/Exercise8.py
+++ ExerciseSumAssess1/Exercise8.py
@@ -1,7 +1,8 @@
-names = ("Jake" "Zac", "Ian", "Ron", "Sam", "Dave")
+names = ("Jake", "Zac", "Ian", "Ron", "Sam", "Dave")
 search = input('Enter the name you are looking for: ')
 
 if search.capitalize() in names:
     print (f'{search} is in the list')
 else:
+
     print (f'{search} is not in the list')
